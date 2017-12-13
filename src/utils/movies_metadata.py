6f9b312e52e71c5993a3f62d4e89c31cad7ad4ab
Yannis Xarchakos <zannisxarchakos@hotmail.com>
2017-12-13 18:05:51 -0500

    movies metadata

--- src/utils/movies_metadata.py
+++ src/utils/movies_metadata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime
+import pickle
 
 
 def load_cast_crew():
@@ -19,8 +20,7 @@ def load_cast_crew():
                         movies_crew.append({str(crew["id"]): crew["job"]})
             movies_data[id]["actors"] = movies_actors
             movies_data[id]["crew"] = movies_crew
-            print movies_data[id]
-            exit()
+
     return movies_data
 
 
@@ -33,7 +33,7 @@ def load_movies():
             d = datetime.strptime(str(s[14]), '%Y-%m-%d')
             if 1990 <= d.year <= 2016 and 'Documentary' not in str(s[3]) and 'en' in str(s[17]) and float(s[15]) > 0 and float(s[22]) > 0:
                 mov = dict()
-                mov["name"] = s[7]
+                mov["name"] = s[8]
                 mov["year"] = d.year
                 mov["revenue"] = float(s[15])
                 mov["rating"] = float(s[22])
@@ -49,18 +49,47 @@ def load_people():
     movies_data = load_movies()
     movies = pd.read_csv(r"../../data/credits.csv").as_matrix()
     actors = dict()
+    crew = dict()
     for movie in movies:
-        if movie[2] in movies_data:
-            movies_actors = list()
+        if str(movie[2]) in movies_data:
             for actor in eval(movie[0]):
-                movies_actors[actor["id"]] = {"name": actor["name"], "movies": set()}
+                actors[actor["id"]] = {"name": actor["name"], "movies": set()}
+    for movie in movies:
+        if str(movie[2]) in movies_data:
+            for actor in eval(movie[0]):
+                actors[actor["id"]]["movies"].add(str(movie[2]))
 
     for movie in movies:
-        if movie[2] in movies_data:
-            movies_actors = list()
-            for actor in eval(movie[0]):
-                movies_actors[actor["id"]]['movies'].add(movie[2])
-                print movies_actors[actor["id"]]
-                exit()
+        if str(movie[2]) in movies_data:
+            for c in eval(movie[1]):
+                if 'Producer' == c['job'] or 'Director' == c['job'] or 'Writing' == c['department']:
+                    crew[c["id"]] = {"name": c["name"], "movies": dict()}
 
-load_people()
+
+
+    for movie in movies:
+        if str(movie[2]) in movies_data:
+            for c in eval(movie[1]):
+                if 'Producer' == c['job'] or 'Director' == c['job'] or 'Writing' == c['department']:
+                    crew[c["id"]]["movies"][str(movie[2])] = []
+
+    for movie in movies:
+        if str(movie[2]) in movies_data:
+            for c in eval(movie[1]):
+                if 'Producer' == c['job'] or 'Director' == c['job'] or 'Writing' == c['department']:
+                    if c['department'] == 'Writing':
+                        crew[c["id"]]["movies"][str(movie[2])].append('Writer')
+                    else:
+                        crew[c["id"]]["movies"][str(movie[2])].append(c['job'])
+
+    return actors, crew
+
+
+actors, crew = load_people()
+movies = load_cast_crew()
+
+folder = "../../dicts/"
+
+pickle.dump(actors, open(folder + "actors.p", 'wb'))
+pickle.dump(crew, open(folder + "crew.p", 'wb'))
+pickle.dump(movies, open(folder + "movies.p", 'wb'))
