6c46abdb1c25b8c9c451171d478fc2dc438bdee0
Yannis Xarchakos <zannisxarchakos@hotmail.com>
2017-12-14 17:59:27 -0500

    relative path

--- src/feature_extraction/utils.py
+++ src/feature_extraction/utils.py
@@ -1,12 +1,15 @@
 import pickle
+import os
+project_folder = os.path.dirname(__file__).split("src")[0]
 
 
 class Feature:
 
     def __init__(self):
-        self.movies = pickle.load(open('../../../dicts/movies.p'))
-        self.actors = pickle.load(open('../../../dicts/actors.p'))
-        self.crew = pickle.load(open('../../../dicts/crew.p'))
+        self.movies = pickle.load(open(project_folder + 'dicts/movies.p'))
+        self.actors = pickle.load(open(project_folder + 'dicts/actors.p'))
+        self.crew = pickle.load(open(project_folder + 'dicts/crew.p'))
+
     def load_desired_movies(self, year, type="exact"):
         id_list = list()
         if type == "exact":
