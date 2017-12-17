4cb6255bef52e28b553b285e68d2f59b51f8109a
Yannis Xarchakos <zannisxarchakos@hotmail.com>
2017-12-17 14:52:56 -0500

    feature extraction

--- src/feature_extraction/extraction_skeleton.py
+++ src/feature_extraction/extraction_skeleton.py
@@ -2,6 +2,7 @@ from src.feature_extraction.utils import Feature
 from src.feature_extraction.normal_features.features import Normal_Features
 import operator
 import numpy as np
+import collections
 import math
 
 class Feature_Extraction:
@@ -20,31 +21,28 @@ class Feature_Extraction:
 
     def sort_by_year(self):
         temp = dict()
-        for key, value in sorted(self.movies.iteritems(), key=lambda (k, v): (v, k)):
-            print "%s: %s" % (movies[key]['name'], value)
-
-
+        sorted_dict = collections.OrderedDict()
         for m_id, movie in self.movies.iteritems():
             if movie["year"] >= 1995:
                 temp[m_id] = movie["year"]
-
-        return sorted(temp.items(), key=operator.itemgetter(1))
+        for key in sorted(temp.items(), key=operator.itemgetter(1)):
+            sorted_dict[key[0]] = key[1]
+        return sorted_dict
 
     def movies_per_year(self, year, feature_type="exact"):
-
-        print self.nf.avg_team_aggregate_value(year, feature_type, "rating")
-
-        for m_id in self.sorted_movies:
-
+        feature_dict = dict()
+        f1 = self.nf.avg_team_aggregate_value(year, feature_type, "rating")
+        for key, y in self.sorted_movies:
+            if y == year:
+                feature_dict[key] = f1[key]
 
     def create_target_vector(self):
         y = []
-        for t in self.sorted_movies:
-            id = t[0]
-            rounded = int(round(self.movies[id]["rating"]))
+        for key in self.sorted_movies.keys():
+            rounded = int(round(self.movies[key]["rating"]))
             y.append(rounded)
         return np.array(y)
 
 
 k = Feature_Extraction()
-k.movies_per_year(1995)
+k.create_target_vector()
